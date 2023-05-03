01394bbc327332146b828b28e06c63ddb4093638
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-05-03 13:03:57 -0400

    updated /models/thought.js to have better validation using minlength and maxlength updated createdAt properties so that date would be correctly formated, installed package moment.js to do this. Created a virtual in /models/thought.js to return the reactionCount by calling .length on the reaction property of the thought. updated /models/user.js to have a function which validates the email, added trim property to username and email, added match property with a regex expression to email, of userSchema, wrapped thought and friends in square array brackets, added in a virtual that returns the friend count by calling .length on the frinds property of the userschema.

--- models/thought.js
+++ models/thought.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Moment = require("moment");
 
 const userReaction = new mongoose.Schema({
   reaction_id: {
@@ -10,6 +11,8 @@ const userReaction = new mongoose.Schema({
   reaction_content: {
     type: String,
     require: true,
+    minlength: 1,
+    maxlength: 280,
   },
   username: {
     type: String,
@@ -18,25 +21,41 @@ const userReaction = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now,
+    get: (createdDate) => Moment(createdDate).format("MMM DD, YYYY"),
     immutable: true,
   },
 });
 
-const userThought = new mongoose.Schema({
-  thought_content: {
-    type: String,
-    require: true,
+const userThought = new mongoose.Schema(
+  {
+    thought_content: {
+      type: String,
+      require: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdDate) => Moment(createdDate).format("MMM DD, YYYY"),
+      immutable: true,
+    },
+    reaction: [userReaction],
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    defualt: () => Date.now(),
-    immutable: true,
-  },
-  reaction: [userReaction],
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+userThought.virtual("reactionCount").get(function () {
+  return this.reaction.length;
 });
 
 const Thought = mongoose.model("Thought", userThought);
--- models/user.js
+++ models/user.js
@@ -1,24 +1,54 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    reqired: true,
+const validateEmail = (email) => {
+  const et = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return et.test(email);
+};
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      reqired: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      required: "Email address is required",
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    friends: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User",
+      },
+    ],
+    thought: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "Thought",
+      },
+    ],
   },
-  email: {
-    type: String,
-    unique: true,
-    require: "true",
-  },
-  friends: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "User",
-  },
-  thought: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Thought",
-  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length
 });
 
 const User = mongoose.model("User", userSchema);
